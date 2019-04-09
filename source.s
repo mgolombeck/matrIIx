ff13d9fdd1cdc1dc5f56b576a674d86ef6a6fb97
Marc A. Golombeck <marc-andre.golombeck@web.de>
2019-04-09 22:57:57 +0200

    Update source.s

--- source.s
+++ source.s
@@ -1,28 +1,28 @@
 MAIN
-					LDA	  bMark
-					BNE	  mark2
-mark1			JSR	  DisplayMatrix
-					LDA	  KYBD				; loop until keypress
-					BMI 	END
-					JSR	  DisplayGuys
-					LDA	  KYBD				; loop until keypress
-					BMI 	END
-					JSR	  DisplayMatrix
-					LDA	  KYBD				; loop until keypress
-					BMI 	END
-					JSR	  DisplayGuys
-					LDA	  KYBD				; loop until keypress
-					BMI 	END
-					JSR	  DisplayMatrix
-					LDA	  KYBD				; loop until keypress
-					BMI 	END
+		LDA	bMark
+		BNE	mark2
+mark1		JSR	DisplayMatrix
+		LDA	KYBD				; loop until keypress
+		BMI 	END
+		JSR	DisplayGuys
+		LDA	KYBD				; loop until keypress
+		BMI 	END
+		JSR	DisplayMatrix
+		LDA	KYBD				; loop until keypress
+		BMI 	END
+		JSR	DisplayGuys
+		LDA	KYBD				; loop until keypress
+		BMI 	END
+		JSR	DisplayMatrix
+		LDA	KYBD				; loop until keypress
+		BMI 	END
 					
 bw1     	LDA 	bMark				; wait for next part
         	BEQ 	bw1
-        	JMP	  MAIN				
+        	JMP	MAIN				
 					
-mark2			JSR	  DisplayRF
+mark2		JSR	DisplayRF
 
-					LDA	  KYBD				; loop until keypress
-					BMI 	END
-					JMP	  MAIN
+		LDA	KYBD				; loop until keypress
+		BMI 	END
+		JMP	MAIN
